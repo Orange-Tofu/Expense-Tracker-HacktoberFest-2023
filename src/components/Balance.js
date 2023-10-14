5aab5756d6ae351d4710eaea689961a3c545f9ba
Anemaygi <gianemayumisan@gmail.com>
2023-10-14 01:50:04 -0300

    round balance to 2 decimal places

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -8,7 +8,7 @@ const BalanceText = styled(Typography)`
 const Balance = ( {transactions} ) =>{
 
     const amount = transactions.map(transaction => transaction.amount);
-    const total = amount.reduce((acc,item) => (acc += item),0);
+    const total = amount.reduce((acc,item) => (acc += item),0).toFixed(2);
 
 
     return(
