5bef03ab35d4231491d35ee99d2e954d1707e486
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2023-10-14 22:29:29 +0530

    balance_going_negative_resolved

--- src/components/NewTransactions.js
+++ src/components/NewTransactions.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
-
-import { Box, Typography, TextField, Button, styled } from "@mui/material";
+import { Box, Typography, TextField, Button, styled, Alert } from "@mui/material";
 
 const Container = styled(Box)`
   display: flex;
@@ -16,27 +15,50 @@ const Container = styled(Box)`
 
 const NewTransactions = ({ setTransactions }) => {
   const [text, setText] = useState("");
-  const [amount, setAmount] = useState();
+  const [amount, setAmount] = useState("");
+  const [balance, setBalance] = useState(47920);
+  const [error, setError] = useState(null);
 
   const addTransaction = () => {
-    const transaction = {
-      id: Math.floor(Math.random() * 100000),
-      text: text,
-      amount: +amount,
-    };
-    setTransactions((prevState) => [transaction, ...prevState]);
+    const transactionAmount = parseFloat(amount); // Use parseFloat to ensure a valid number
+    if (isNaN(transactionAmount)) {
+      setError("Please enter a valid number for the amount.");
+    } else {
+      const newBalance = balance + transactionAmount;
+      console.log(balance);
+      console.log(transactionAmount);
+      console.log(newBalance);
+      if (newBalance < 0) {
+        setError("You don't have enough balance.");
+      } else {
+        const transaction = {
+          id: Math.floor(Math.random() * 100000),
+          text: text,
+          amount: transactionAmount,
+        };
+        setTransactions((prevState) => [transaction, ...prevState]);
+        setBalance(newBalance);
+        setError(null);
+        setText("");
+        setAmount("");
+      }
+    }
   };
+
   return (
     <Container>
-      <Typography variant="h4"> New Transaction </Typography>
+      <Typography variant="h4">New Transaction</Typography>
+      {error && <Alert severity="error">{error}</Alert>}
       <TextField
         variant="filled"
         label="Enter Transaction"
+        value={text}
         onChange={(e) => setText(e.target.value)}
       />
       <TextField
         variant="filled"
         label="Enter Amount"
+        value={amount}
         onChange={(e) => setAmount(e.target.value)}
       />
       <Button variant="contained" onClick={() => addTransaction()}>
@@ -45,4 +67,5 @@ const NewTransactions = ({ setTransactions }) => {
     </Container>
   );
 };
+
 export default NewTransactions;
